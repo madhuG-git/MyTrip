ffdecd48715cbaa26ca7d1c2bb2112509eb840a6
madhuG-git <usersudhan235@gmail.com>
2026-08-18 13:27:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const methodoverride = require("method-override")
 const ejsmate = require("ejs-mate")
 const wrapAsync = require("./utils/wrapAsync")
 const ExpressError = require("./utils/ExpressError")
-
+const cookieparser = require("cookie-parser")
+const session = require("express-session")
+const flash = require("connect-flash")
 
 app.listen(8080,()=> {
     console.log("Server is listening on port 8080")
@@ -23,12 +25,34 @@ connectdb().then(()=> {
 
 const listing = require("./models/listingsch")
 const review = require("./models/reviews")
+require('dotenv').config()
 app.set("view engine","ejs")
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodoverride("_method"));
 app.engine("ejs",ejsmate)
+app.use(cookieparser())
+
+const sessionOptions = {
+    secret : process.env.SESSIONSECRET,
+    resave : false,
+    saveUninitialized : true,
+    cookie : {
+        maxAge : 60*60*1000,
+        httpOnly : true,
+    }
+}
+
+app.use(session(sessionOptions))
+app.use(flash())
+
+app.use((req,res,next)=> {
+    res.locals.success = req.flash("success")
+    res.locals.error = req.flash("error")
+    next()
+})
+
 
 //routes
 
@@ -48,6 +72,10 @@ app.get("/listings/new",(req,res)=> {
 app.get("/listings/:id",wrapAsync(  async (req,res)=> {
     const {id} = req.params;
     const data = await listing.findById(id).populate("reviews")
+    if(!data) {
+        req.flash("error","Listing you request for does not exist!")
+       return res.redirect("/listings")
+    }
     res.render("listings/show",{data})
 }))
 
@@ -57,12 +85,17 @@ app.post("/listings",wrapAsync(  async (req,res)=> {
         throw new ExpressError(400,"send valid data for listing")
     }
     await listing.insertOne({title:title,description:description,image:image,price:price,location:location,country:country})
+    req.flash("success","New Listing Created")
     res.redirect("/listings")
 }))
 
 app.get("/listings/:id/edit", wrapAsync( async (req,res)=> {
     const {id} = req.params;
     const data = await listing.findById(id)
+    if(!data) {
+        req.flash("error","Listing you request for does not exist!")
+       return res.redirect("/listings")
+    }
     res.render("listings/edit" ,{data})
 }))
 
@@ -73,6 +106,7 @@ app.put("/listings/:id",wrapAsync( async (req,res)=> {
         throw new ExpressError(400,"send valid data for listing")
     }
     await listing.findByIdAndUpdate(id,{title:title,description:description,image:image,price:price,location:location,country:country})
+    req.flash("success","Listing Updated")
     res.redirect(`/listings/${id}`)
 
 }))
@@ -80,6 +114,7 @@ app.put("/listings/:id",wrapAsync( async (req,res)=> {
 app.delete("/listings/:id",wrapAsync( async (req,res) => {
     const {id} = req.params
     await listing.findByIdAndDelete(id)
+    req.flash("success","Listing Deleted")
     res.redirect("/listings")
 }))
 
@@ -93,6 +128,7 @@ app.post("/listings/:id/review",wrapAsync(async(req,res)=> {
     await newreview.save()
     place.reviews.push(newreview)
     await place.save()
+    req.flash("success","New Review Created")
     res.redirect(`/listings/${id}`)
 }))
 
@@ -100,8 +136,10 @@ app.delete("/listings/:id/review/:reviewid",wrapAsync(async(req,res)=> {
     let {id,reviewid} = req.params
     await listing.findByIdAndUpdate(id,{$pull: {reviews : reviewid}})
     await review.findByIdAndDelete(reviewid)
+    req.flash("success","Review Deleted")
     res.redirect(`/listings/${id}`)
 }))
+
 
 app.use((req,res,next)=> {
     throw new ExpressError(404,"Page Not Found")
